0dd362dfa94d8260431b743576099967e29d3040
TheHighFish <thehighfish@googlemail.com>
2018-07-15 14:05:32 +0200

    starting GamestatePopulator.DLL

---
+++ DLLs/OpenHoldem_GamestatePopulator_DLL/CLazyScraper.h
@@ -0,0 +1,50 @@
+#pragma once
+//******************************************************************************
+//
+// This file is part of the OpenHoldem project
+//    Source code:           https://github.com/OpenHoldem/openholdembot/
+//    Forums:                http://www.maxinmontreal.com/forums/index.php
+//    Licensed under GPL v3: http://www.gnu.org/licenses/gpl.html
+//
+//******************************************************************************
+//
+// Purpose:
+//
+//******************************************************************************
+
+///#include "CSpaceOptimizedGlobalObject.h"
+
+class CLazyScraper /*#: public CSpaceOptimizedGlobalObject */{
+ public:
+	CLazyScraper();
+	~CLazyScraper();
+ public:
+	void DoScrape();
+  // This function should be preferred
+  // and not cScraper::IsIdenticalScrape()
+  // because this one just caches the function result.
+  bool IsIdenticalScrape()  { return _is_identical_scrape; }
+ private:
+	bool NeedDealerChair();
+	bool NeedHandNumber();
+	bool NeedUsersCards();
+	bool NeedAllPlayersCards();
+	bool NeedFoldButton();
+	bool NeedActionbuttons();
+	bool NeedInterfaceButtons();
+	bool NeedBetpotButtons();
+	bool NeedSlider();
+	bool NeedBetsAndBalances();
+	bool NeedAllPlayerNames();
+	bool NeedUnknownPlayerNames();
+	bool NeedCommunityCards();
+  bool NeedColourCodes();
+  bool NeedMTTRegions();
+ private:
+	void ScrapeUnknownPlayerNames();
+ private:
+	bool CardScrapeNeeded();
+	bool CompleteScrapeNeeded();
+ private:
+  bool _is_identical_scrape;
+};
---
+++ DLLs/OpenHoldem_GamestatePopulator_DLL/COpenHoldemGamestatePopulator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class COpenHoldemGamestatePopulator {
+public:
+  COpenHoldemGamestatePopulator();
+  ~COpenHoldemGamestatePopulator();
+public:
+  void Populate();
+};
